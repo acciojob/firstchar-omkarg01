0406d40e20dd93cba45b3d9aaedf6995801d21d0
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2024-07-05 20:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function firstChar(text) {
   // your code here
-	return text.toLowerCase() + "-" + text.toUpperCase();
+	// return text.toLowerCase() + "-" + text.toUpperCase();
+	let str = text.replace(/ /g,'');
+	if (str.length == 0){
+		return ''
+	} else {
+		return str[0]
+	}
+	
 }
 
 // Do not change the code below
